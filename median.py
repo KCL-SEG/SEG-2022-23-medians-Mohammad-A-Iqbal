be7a3ed9dcb71318fed1f2c347967ec3bfc7f22f
Mohammad A Iqbal <nea.testing.coursework@gmail.com>
2022-11-28 17:18:13 +0000

    Done

--- median.py
+++ median.py
@@ -9,4 +9,28 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+
+int_numbers = [int(i) for i in numbers]    
+
+int_numbers.sort()
+
+length = len(int_numbers)
+
+test = length%2
+
 print(numbers)
+median  = "The median for the list is: "
+
+if test == 1:
+    median += str((int_numbers[int((length - 1)/2)]))
+
+else:
+    median1 = int_numbers[int((length - 1)/2)]
+    median2 = int_numbers[int(((length - 1)/2) + 1)]
+    median += str((median1 + median2)/2)
+
+
+
+print(median)
+
+
